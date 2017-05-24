2774aa19f86a999421158dde2e2202efeece7728
SnaileeWhale <kvile83@gmail.com>
2017-05-24 09:55:23 +0800

    Upload from current working version
    multiple changes

--- patch-scanning.ps1
+++ patch-scanning.ps1
@@ -1,33 +1,24 @@
 <#
 
-
-
-
 -----------------------------------------
-
-PowerShell Script 
 
 Patch Scanning Utility 
+-targets either 2008r2 or 2012r2 for a specified KB 
 
 -----------------------------------------
-
 #>
 
 #Environmentals
 
-$smtpserver = #Enter smtp server
-$Email = #Enter EMail Address 
-$filelocation = C:\temp\Patched.CSV #Enter File Location
 $OutputArray = @()
+$filelocation = "C:\Temp\Patch-Report.csv"
+$names = Get-content C:\text\computers.txt # breakout text array for testing 
 
-
-#loop start
 
 do
 {
 
-
-#switch loop
+#
 
 write-host " "
 write-host "Server Patch Scanning utility" 
@@ -41,41 +32,46 @@ $OperatingSystem = Read-Host " "
 
 
 
-if(($OperatingSystem -eq "1") -or ($OperatingSystem -eq "2"))
+    if(($OperatingSystem -eq "1") -or ($OperatingSystem -eq "2") -or ($OperatingSystem -eq "3"))
+    {
 
-{
+        switch($OperatingSystem)
+        {
+            "1"
+            {
 
-switch($OperatingSystem)
-{
-"1"
-{
+            $2012r2 = Get-ADComputer -Filter {OperatingSystem -like "*Windows Server 2012 R2*"} -Properties Operatingsystem | select Name, OperatingSystem | sort name
 
-$2012r2 = Get-ADComputer -Filter {OperatingSystem -like "*Windows Server 2012 R2*"} -Properties Operatingsystem | select Name, OperatingSystem | sort name
+            $names = $2012r2.Name 
 
-$names = $2012r2.Name 
-
-}
-"2"
-{
-
-
-$2008r2 = Get-ADComputer -Filter {OperatingSystem -like "*Windows Server 2008 R2*"} -Properties Operatingsystem | select Name, OperatingSystem | sort name
-
-$names = $2008r2.Name
-
-}
+            $ServerType = "2012r2"
+            }
+            "2"
+            {
 
 
-}
+            $2008r2 = Get-ADComputer -Filter {OperatingSystem -like "*Windows Server 2008 R2*"} -Properties Operatingsystem | select Name, OperatingSystem | sort name
 
+            $names = $2008r2.Name
 
+            $ServerType = "2008r2"
+            }
+            "3"
+            {
+            $names = Get-content E:\computers.txt
+
+            $ServerType = "Textfile"
+            }
+
+        }
+#
 
 $patchID = Read-host "Enter Patch KB" 
 
 
 
 
-#For each loop
+#Start loop
 
 foreach ($name in $names)
 
@@ -90,7 +86,7 @@ foreach ($name in $names)
             {
 
 
-                    Write-Host "$name is connected" -ForegroundColor Green
+                    $connectionOK = Write-Host -NoNewline "$name`t`t`tOnline" -ForegroundColor Green
 
                     $connected = "Yes"
 
@@ -104,10 +100,13 @@ foreach ($name in $names)
 
                                     {
 
-                                    Write-Host "---$name is patched" -ForegroundColor Green
+                                    
+                                    $PatchedOK = Write-Host "`t`t$Patched" -ForegroundColor Green
 
                                     $Patched = "Yes"
 
+                                    $connectionOK
+                                    $PatchedOK
 
                                     }
 
@@ -115,9 +114,12 @@ foreach ($name in $names)
 
                                     {
 
-                                    Write-Host "---$name is not patched" -ForegroundColor Red
+                                    $PatchedNotOK = Write-Host "`t`tNot patched" -ForegroundColor Red
 
                                     $Patched = "No"
+
+                                    $connectionOK
+                                    $PatchedNotOK
 
                                     }
 
@@ -126,7 +128,7 @@ foreach ($name in $names)
                           Catch
 
                           {
-                          Write-Host "---$name is not responding to query possibly firewalled,WinRM not Enabled or RPC error" -ForegroundColor yellow
+                          Write-Host "`t`tNot responding to query, possible firewall, invalid patch id, WinRM or RPC error" -ForegroundColor yellow
 
                           $Patched = "Unknown"
 
@@ -138,7 +140,7 @@ foreach ($name in $names)
  
              {
 
-             Write-Host "$name is not online" -ForegroundColor darkred
+             Write-Host "$name`t`t`tOffline" -ForegroundColor darkred
 
              $connected = "No"
              $patched = "Unknown"
@@ -153,24 +155,107 @@ $OutputArray += $ServerObject
   
 }
 
+
 $OutputArray | sort patched | FT -AutoSize
 
-$OutputArray | sort patched | Export-Csv $filelocation -NoTypeInformation
+do{
 
-send-mailmessage -From $Email -subject "Machines missing patch $PatchID " -To $Email -Attachments $filelocation -smtpserver $smtpserver 
+        try
+
+            {
+            $OutputArray | sort patched | Export-Csv $filelocation -NoTypeInformation
+            write-host " "
+            Write-host "Exported CSV to C:\Temp\Patch-Report.csv" -ForegroundColor green
+            write-host " "
+            
+            
+            
+            
+            
+            
+            
+            
+      try
+            {
+
+$SendEmail = Read-host "Send Email? (y/n)?"
+
+    if($SendEmail -eq "y")
+            {
+            write-host " "
+            $emailaddress = Read-host "Enter Email Address"
+            write-host " "
+            $smtp = Read-host "Enter SMTP Mail Server e.g 'mail'" 
+
+            send-mailmessage -From $emailaddress -subject "Server $ServerType machines missing $patchID" -To $emailaddress -Attachments $filelocation -smtpserver $smtp 
+
+            write-host " "
+            "Email Sent"
+            write-host " "
+            $retry = "n"
+
+            }
+
+
+
+    else
+            {
+            write-host " "
+            "No Email Sent"
+            write-host " "
+            $retry = "n"
+            }
+            
+            }
+
+
+            
+
+       Catch
+            {
+            write-host " "
+            Write-host "Error invalid SMTP Server" -ForegroundColor Red
+            write-host " "
+            $retry = read-host "Retry? (y/n)"
+            write-host " "
+            }
+            
+            
+            
+            }
+
+
+       Catch
+            {
+            write-host " "
+            write-host "Error Exporting to C:\Temp\Patch-Report.csv" -ForegroundColor Red
+            write-host " "
+            $retry = read-host "Retry? (y/n)"
+            write-host " "
+            }
+
+
+
+}
+while($retry -eq "y")
+
 
 }
 
-else
-{
-""
-"Invalid Selection"
-""}
+
+    else
+            {
+            write-host " "
+            "Invalid Selection"
+            write-host " "
+            }
 
 
 
 $repeat = Read-Host "Exit? (y/n)"
-
-
 }
+
 while($repeat -ne "y")
+
+write-host " "
+write-host "Exiting..."
